5722109cbcf604df2fad2c1543e712b2e6483903
kileruncio <kileruncio@pop-os.localdomain>
2022-03-14 22:46:15 +0100

    Nothing

--- src/App.java
+++ src/App.java
@@ -1,5 +1,34 @@
+import java.util.Scanner;
+
 public class App {
-    public static void main(String[] args) throws Exception {
-        System.out.println("Hello, World!");
+    public static void main(String[] args){
+        Scanner scanner = new Scanner(System.in);
+
+        System.out.println("Prosze podac n: ");
+        
+        int n = scanner.nextInt();
+        scanner.close();
+
+        int sum = 0;
+
+        for(int i = 1; i < n; i++){
+            int j = 0;
+            int a = 1;  
+            switch(i%4){
+                case 0:
+                    
+                    break;
+                case 1:
+                    break;
+                case 2:
+                    break;
+                case 3:
+                    break;
+                
+            }
+
+            for(int q = 1; q < i; q++) a *= q;
+
+        }
     }
 }
